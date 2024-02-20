a68b7c121df713b46cb433ee7bc098662acc9dc8
Mishok1 <mm21112000@gmail.com>
2024-02-20 10:55:09 +0300

    Refactoring

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,4 @@
-const stringLength = (string, maxLength) => {
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return false;
-};
+const checkLength = (aString, maxLength) => aString.length <= maxLength;
 
 const palindrome = (string) => {
   string = string.replaceAll(' ','');
@@ -13,12 +8,7 @@ const palindrome = (string) => {
   for (let i = string.length - 1; i >= 0; i--) {
     resultString += string[i];
   }
-
-  if (string === resultString) {
-    return string === resultString; //Явно не так, но до другого я не додумался.
-  }
-
-  return false;
+  return string === resultString;
 };
 
 const findNumbers = (string) => {
@@ -31,10 +21,10 @@ const findNumbers = (string) => {
       number += string[i];
     }
   }
-  return number;
+  return typeof(+ number);
 };
 
 
-stringLength('adfgd', 4);
+checkLength('adfad', 6);
 palindrome('aga bgb Aga');
 findNumbers('a2ad0k0.5k2ld2');
