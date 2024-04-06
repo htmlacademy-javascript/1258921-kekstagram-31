b9ae275771e6228926c5c0e4347d21a334099cdd
Mishok1 <mm21112000@gmail.com>
2024-04-06 23:29:04 +0300

    Finish imageOpen

--- js/imageOpen.js
+++ js/imageOpen.js
@@ -1,9 +1,52 @@
 import { isEscapeKey, isEnterKey } from './functions.js';
+import { photosArray } from './data.js';
 import './thumbnails.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
-const picture = document.querySelector('.picture');
+const pictures = document.querySelectorAll('.picture');
+
+
+for (let i = 0; i < pictures.length; i++) {
+  pictures[i].dataset.id = i + 1;
+}
+
+const commentFragment = document.createDocumentFragment();
+const commentsContainer = document.querySelector('.social__comments');
+
+
+function makeNewComment(array) {
+  array.forEach((comment) => {
+    const commentTemplate = document.querySelector('.social__comment').cloneNode(true);
+    commentTemplate.querySelector('.social__picture').src = comment.avatar;
+    commentTemplate.querySelector('.social__picture').alt = comment.name;
+    commentTemplate.querySelector('.social__text').textContent = comment.message;
+    commentFragment.append(commentTemplate);
+  });
+}
+
+pictures.forEach((element) =>
+  element.addEventListener('click', () => {
+
+    const currentId = element.dataset.id;
+    const currentObject = photosArray[currentId - 1];
+    document.querySelector('.big-picture__img img').src = currentObject.url;
+    document.querySelector('.likes-count').textContent = currentObject.likes;
+    document.querySelector('.social__comment-total-count').textContent = currentObject.comments.length;
+    makeNewComment(currentObject.comments);
+    commentsContainer.innerHTML = '';
+    commentsContainer.appendChild(commentFragment);
+    document.querySelector('.social__comment-shown-count').textContent = document.querySelector('.social__comments').children.length;
+    document.querySelector('.social__caption').textContent = currentObject.description;
+    openPicture();
+  })
+);
+
+pictures.forEach((element) =>
+  element.addEventListener('click', () => {
+    closePicture();
+  })
+);
 
 function openPicture() {
   bigPicture.classList.remove('hidden');
@@ -14,8 +57,11 @@ function openPicture() {
       bigPicture.classList.add('hidden');
     }
   });
-
+  document.querySelector('.social__comment-count').classList.add('hidden');
+  document.querySelector('.comments-loader').classList.add('hidden');
+  document.querySelector('body').classList.add('modal-open');
 }
+
 
 function closePicture() {
   bigPictureCancel.addEventListener('click', () => {
@@ -27,14 +73,5 @@ function closePicture() {
       bigPicture.classList.add('hidden');
     }
   });
+  document.querySelector('body').classList.remove('modal-open');
 }
-
-picture.addEventListener('click', () => {
-  openPicture();
-});
-
-picture.addEventListener('click', () => {
-  closePicture();
-});
-
-
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,11 +4,9 @@ const picturesSection = document.querySelector('.pictures');
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const newPicture = photosArray;
-
 const pictureFragment = document.createDocumentFragment();
 
-newPicture.forEach((photoData) => {
+photosArray.forEach((photoData) => {
   const picture = pictureTemplate.cloneNode(true);
   picture.querySelector('.picture__img').src = photoData.url;
   picture.querySelector('.picture__img').alt = photoData.description;
